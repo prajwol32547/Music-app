ab5ae445628e9d284ee696731b3720d5d5d78aef
prajwol lama <73230778+prajwol32547@users.noreply.github.com>
2023-11-13 23:59:45 +0545

    Update NowPlaying.dart

--- lib/NowPlaying.dart
+++ lib/NowPlaying.dart
@@ -13,12 +13,21 @@ class NowPlaying extends StatefulWidget {
 }
 
 class NowState extends State<NowPlaying> {
-  bool _isplaying = false;
+  bool _isPlaying = false;
+  double _sliderValue = 0.0;
 
   @override
-  void init() {
+  void initState() {
     super.initState();
     playNow();
+
+    // Listen to the position stream and update the slider value
+    widget.audioplayer.positionStream.listen((Duration position) {
+      setState(() {
+        // Calculate the percentage of audio played
+        _sliderValue = position.inMilliseconds / widget.songModel.duration!;
+      });
+    });
   }
 
   void playNow() {
@@ -26,9 +35,11 @@ class NowState extends State<NowPlaying> {
       widget.audioplayer
           .setAudioSource(AudioSource.uri(Uri.parse(widget.songModel.uri!)));
       widget.audioplayer.play();
-      _isplaying = true;
-    } on Exception {
-      log('cannot play songs');
+      setState(() {
+        _isPlaying = true;
+      });
+    } catch (e) {
+      log('Error playing song: $e');
     }
   }
 
@@ -46,114 +57,138 @@ class NowState extends State<NowPlaying> {
       body: Padding(
         padding: const EdgeInsets.only(top: 50),
         child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Center(
-              child: Column(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.amber,
-                    child: Icon(
-                      Icons.music_note,
-                      size: 80.0,
-                    ),
-                    radius: 100.0,
+          height: MediaQuery.of(context).size.height,
+          child: Center(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.amber,
+                  child: Icon(
+                    Icons.music_note,
+                    size: 80.0,
                   ),
-                  SizedBox(height: 30),
-                  SizedBox(
-                    height: 60,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        widget.songModel.displayNameWOExt,
-                        overflow: TextOverflow.fade,
-                        maxLines: 1,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    height: 50,
+                  radius: 100.0,
+                ),
+                SizedBox(height: 30),
+                SizedBox(
+                  height: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: Text(
-                      widget.songModel.artist.toString() == "<unknown>"
-                          ? "Prajwol"
-                          : widget.songModel.artist.toString(),
-                      overflow: TextOverflow.fade,
+                      widget.songModel.displayNameWOExt,
+                      overflow: TextOverflow.clip,
                       maxLines: 1,
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  SizedBox(
-                    height: 60,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        children: [
-                          Text("0.00"),
-                          Expanded(
-                            child: Slider(
-                              min: 0,
-                              max: 5,
-                              value: 0.7,
-                              onChanged: (value) {},
-                              thumbColor: Colors.white,
-                              activeColor: Colors.red,
-                              inactiveColor: Colors.blue,
-                            ),
-                          ),
-                          Text("5.00")
-                        ],
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
                       ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  SizedBox(
-                      height: 40,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.skip_previous_outlined,
-                                size: 40,
-                              )),
-                          IconButton(
-                              onPressed: () {
-                                setState(() {
-                                  _isplaying = !_isplaying;
-                                  if (_isplaying) {
-                                    widget.audioplayer.play();
-                                  } else {
-                                    widget.audioplayer.pause();
-                                  }
-                                });
-                              },
-                              icon: Icon(
-                                _isplaying == true
-                                    ? Icons.pause_circle_filled_outlined
-                                    : Icons.play_circle_fill_outlined,
-                                size: 40,
-                              )),
-                          IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.skip_next_outlined,
-                                size: 40,
-                              ))
-                        ],
-                      ))
-                ],
-              ),
-            )),
+                ),
+                SizedBox(height: 10),
+                SizedBox(
+                  height: 50,
+                  child: Text(
+                    widget.songModel.artist.toString() == "<unknown>"
+                        ? "Prajwol"
+                        : widget.songModel.artist.toString(),
+                    overflow: TextOverflow.fade,
+                    maxLines: 1,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                SizedBox(
+                  height: 60,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Text(_sliderValue.clamp(0.0, 1.0).toStringAsFixed(2)),
+                        Expanded(
+                          child: Slider(
+                            min: 0,
+                            max:
+                                1.0, // Use 1.0 as the maximum value for the slider
+                            value: _sliderValue.clamp(0.0, 1.0),
+                            onChanged: (value) {
+                              _sliderValue = value;
+                              // Handle slider value change (optional)
+                            },
+                            onChangeEnd: (value) {
+                              // Seek to the position when the user stops dragging the slider
+                              final newPosition =
+                                  (value * widget.songModel.duration!).toInt();
+                              widget.audioplayer
+                                  .seek(Duration(milliseconds: newPosition));
+                            },
+                            thumbColor: Colors.black,
+                            activeColor: Colors.black45,
+                            inactiveColor: Colors.white,
+                          ),
+                        ),
+                        Text((widget.songModel.duration! / 60000)
+                            .toStringAsFixed(2)),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.skip_previous_outlined,
+                          size: 40,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _isPlaying = !_isPlaying;
+                            if (_isPlaying) {
+                              widget.audioplayer.play();
+                            } else {
+                              widget.audioplayer.pause();
+                            }
+                          });
+                        },
+                        icon: Icon(
+                          _isPlaying
+                              ? Icons.pause_circle_filled_outlined
+                              : Icons.play_circle_fill_outlined,
+                          size: 40,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.skip_next_outlined,
+                          size: 40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
+
+  // void dispose() {
+  //   widget.audioplayer.dispose();
+  //   super.dispose();
+  // }
 }
